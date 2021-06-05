6e5fb1b138c61c8da2e359855de4db88203fc010
nickjiang <congtao.jiang@outlook.com>
2021-06-05 16:06:41 +0800

    Experiment nested .forEach with Async/Await calls

--- index-urls.js
+++ index-urls.js
@@ -8,14 +8,19 @@ const urls = [
     "https://www.taobao.com/",
     "https://www.npmjs.com/"
 ];
+let range = n => Array.from(Array(n).keys());
 
-urls.forEach(async(url) => {
-    try {
-        let response = await fetch(url, { timeout: 5000 });
-        console.log(`${response.statusText}, ${response.status}, ${url}`);
-    } catch (error) {
-        console.log(`Failed, ${url}`)
-    }
+range(5).forEach(num => {
+    urls.forEach(async(url) => {
+        try {
+            let response = await fetch(url, { timeout: 5000 });
+            console.log(`${num} : ${response.statusText}, ${response.status}, ${url}`);
+        } catch (error) {
+            console.log(`${num} : Failed, ${url}`)
+        }
+    });
 });
+
+
 
 console.log('Program main path completed')
